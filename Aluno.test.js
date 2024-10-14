82f3a188d87e238526bb7800322208c3df1aec2f
Airton Izaki <airton.izaki@iesb.edu.br>
2024-10-14 17:18:33 -0300

    first commit

---
+++ Aluno.test.js
@@ -0,0 +1,26 @@
+﻿const Aluno = require('./Aluno');
+
+test('calcular a média', () => {
+  const aluno = new Aluno('João', [7, 9]);
+  expect(aluno.calcularMedia()).toBe(8);
+});
+
+test('retornar menção', () => {
+  const aluno = new Aluno('Maria', [9, 9]);
+  expect(aluno.obterMencao()).toBe('SS');
+});
+
+test('retorna "aprovad" quando a menção é SS', () => {
+  const aluno = new Aluno('Carlos', [9, 9]);
+  expect(aluno.statusAprovacao()).toBe('aprovad@');
+});
+
+test('retorna "reprovado" quando a menção é MI', () => {
+  const aluno = new Aluno('Ana', [4, 5]);
+  expect(aluno.statusAprovacao()).toBe('reprovad@');
+});
+
+test('lança erro quando a média é maior que 10', () => {
+  const aluno = new Aluno('Pedro', [11, 10]);
+  expect(() => aluno.calcularMedia()).toThrow('Média acima da máxima permitida!');
+});
